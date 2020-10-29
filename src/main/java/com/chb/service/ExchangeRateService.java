c6f1a7ed41831d3963dcddda46831905346ae8cf
zml <745912111@qq.com>
2020-10-29 09:16:14 +0800

    Mybatis

---
+++ src/main/java/com/chb/service/ExchangeRateService.java
@@ -0,0 +1,19 @@
+package com.chb.service;
+
+import com.chb.pojo.ExchangeRate;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface ExchangeRateService {
+    //增加一条汇利率记录
+    int addExchangeRate(ExchangeRate exchangeRate);
+    //删除一条汇利率记录
+    int deleteExchangeRateById(int id);
+    //更新一条汇利率记录
+    int updateExchangeRate(ExchangeRate exchangeRate);
+    //查询一条记录
+    ExchangeRate queryExchangeRateById(int id);
+    //查询全部记录
+    List<ExchangeRate> queryAllExchangeRate();
+}
---
+++ src/main/java/com/chb/service/ExchangeRateServiceImpl.java
@@ -0,0 +1,36 @@
+package com.chb.service;
+
+import com.chb.dao.ExchangeRateMapper;
+import com.chb.pojo.ExchangeRate;
+
+import java.util.List;
+
+public class ExchangeRateServiceImpl implements  ExchangeRateService{
+
+    //service调DAO层：组合Dao
+    private ExchangeRateMapper exchangeRateMapper;
+
+    public void setExchangeRateMapper(ExchangeRateMapper exchangeRateMapper) {
+        this.exchangeRateMapper = exchangeRateMapper;
+    }
+
+    public int addExchangeRate(ExchangeRate exchangeRate) {
+        return exchangeRateMapper.addExchangeRate(exchangeRate);
+    }
+
+    public int deleteExchangeRateById(int id) {
+        return exchangeRateMapper.deleteExchangeRateById(id);
+    }
+
+    public int updateExchangeRate(ExchangeRate exchangeRate) {
+        return exchangeRateMapper.updateExchangeRate(exchangeRate);
+    }
+
+    public ExchangeRate queryExchangeRateById(int id) {
+        return exchangeRateMapper.queryExchangeRateById(id);
+    }
+
+    public List<ExchangeRate> queryAllExchangeRate() {
+        return exchangeRateMapper.queryAllExchangeRate();
+    }
+}
